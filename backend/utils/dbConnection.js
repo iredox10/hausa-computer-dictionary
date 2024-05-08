904fcdaffb8e74be5ddb3c9a214daf5552aa74a0
iredox <idreesadam200@gmail.com>
2024-05-08 14:22:22 +0100

    change path

--- backend/utils/dbConnection.js
+++ backend/utils/dbConnection.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const mongoConnect = async () => {
     try {
         mongoose.set('strictQuery','true')
-        await mongoose.connect('mongodb://localhost/comp-hausa-dictionary')
-        // await mongoose.connect('mongodb+srv://idreesadam200:iredox79@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/comp-hausa-dictionary')
+        // await mongoose.connect('mongodb://localhost/comp-hausa-dictionary')
+        await mongoose.connect('mongodb+srv://idreesadam200:iredox79@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/comp-hausa-dictionary')
         console.log('connect to mongodb')
     } catch (error) {
         console.log(error)
