b2db40659c2532884f9dd9f052d6bed9fd28d70e
iredox <idreesadam200@gmail.com>
2024-05-08 14:18:53 +0100

    fix unique user issue

--- frontend/src/utils/path.js
+++ frontend/src/utils/path.js
@@ -1,2 +1,2 @@
-export const path = 'https://computer-hausa-dictionary-backend.onrender.com'
-// export const path = 'http://localhost:3003'
+// export const path = 'https://computer-hausa-dictionary-backend.onrender.com'
+export const path = 'http://localhost:3003'
